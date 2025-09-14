eadde4764acdc675d7252d5e6c6ff5809b032e92
jonesbit <jonesbit15@gmail.com>
2025-09-13 23:53:48 -0300

    Feat: Adiciona Vercel Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,8 @@ import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 
+import { Analytics } from '@vercel/analytics/react';
+
 import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 config.autoAddCss = false
